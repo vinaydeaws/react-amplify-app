11e61dbf6c0429f7e972266c2788a6699e8242c8
vinaydeaws <120019104+vinaydeaws@users.noreply.github.com>
2023-06-03 21:47:28 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-             <h1>Hello World! </h1>
+             <h1>Hello vinay! </h1>
         <a
           className="App-link"
           href="https://reactjs.org"
